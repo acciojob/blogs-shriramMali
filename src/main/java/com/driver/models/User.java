2b9397481d133467741d0a1c89aef8003d6f94f3
malishrirama17@gmail.com <you@example.com>
2023-01-14 15:48:52 +0530

    k

--- src/main/java/com/driver/models/User.java
+++ src/main/java/com/driver/models/User.java
@@ -1,5 +1,5 @@
 package com.driver.models;
-
+/*
 import javax.persistence.*;
 import java.util.List;
 
@@ -29,8 +29,6 @@ public class User {
         this.lastName = lastName;
         this.firstName = firstName;
     }
-
-
 
 
     public List<Blog> getUserBlogs() {
@@ -82,4 +80,89 @@ public class User {
     }
 
 
+}
+
+ */
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user_table")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    @OneToMany(mappedBy = "user_Info", cascade = CascadeType.ALL)
+    private List<Blog> blogList;
+
+    public User() {
+    }
+
+    public User(String username, String password, String firstName, String lastName, List<Blog> blogList) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.blogList = blogList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Blog> getBlogList() {
+        return blogList;
+    }
+
+    public void setBlogList(List<Blog> blogList) {
+        this.blogList = blogList;
+    }
 }
